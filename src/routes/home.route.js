bb4539864029398cce30e6669247b0bc96f0ecfd
Igashi Michael <mikkybeardless@gmail.com>
2024-04-27 16:10:06 +0100

    edited home text

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -8,8 +8,8 @@ homeRoute.get("/", (req,res)=>{
 
     Use add these extentions to the baseurl to navigate to free routes:
     - /blogs: to get all blogs(only published blogs will be displayed).
-    - /published: returns similarly blogs in the published state.
-    - /:id/read : need a blog id to read and read_count gets updated. For this, here is an id of a blog in published state (6626762ebafe9593f955266e) 
+    - blogs/published: returns similarly blogs in the published state.
+    - blogs/:id/read : need a blog id to read and read_count gets updated. For this, here is an id of a blog in published state (6626762ebafe9593f955266e) 
     
     `})
 })
