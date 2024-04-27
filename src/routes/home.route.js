05d74085380cb136f17f365618b0d16c7f492236
Igashi Michael <mikkybeardless@gmail.com>
2024-04-27 16:00:49 +0100

    add description

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -3,6 +3,14 @@ import { Router } from "express";
 const homeRoute = Router();
 
 homeRoute.get("/", (req,res)=>{
-    res.json({message: "Welcome to Igashi's Blogging API"})
+    res.json({message: `
+    Welcome to Igashi's Blogging API.
+
+    Use add these extentions to the baseurl to navigate to free routes:
+    - /blogs: to get all blogs(only published blogs will be displayed).
+    - /published: returns similarly blogs in the published state.
+    - /:id/read : need a blog id to read and read_count gets updated. For this, here is an id of a blog in published state (6626762ebafe9593f955266e) 
+    
+    `})
 })
 export default homeRoute;
