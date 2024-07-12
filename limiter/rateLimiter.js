947b6279e7e1f65ad6de78d4b9cc7197ddd300ec
Igashi Michael <131265224+Mikkybeardless@users.noreply.github.com>
2024-07-12 09:18:50 +0100

    Update rateLimiter.js

--- limiter/rateLimiter.js
+++ limiter/rateLimiter.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 20 minutes
-  limit: 5, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  limit: 20, // Limit each IP to 20 requests per `window` (here, per 1 minutes).
   keyGenerator: function (req) {
     return req.headers["x-device-id"] || req.ip;
   },
